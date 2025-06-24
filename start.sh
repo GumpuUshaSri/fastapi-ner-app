942d6b321f333d8f7f5735336acd79cb1b46febb
GumpuUshaSri <gumpuushasri@gmail.com>
2025-06-24 22:13:24 +0530

    Fix: remove model download from start.sh, use $PORT

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-python -m spacy download en_core_web_lg
-uvicorn main:app --host 0.0.0.0 --port 10000
+
+# Start the FastAPI app using the port provided by Render
+uvicorn main:app --host 0.0.0.0 --port $PORT
